e9d538dc0f070e6ef4af2eb030736ed749210b9b
CelloSounds <gellsmore@gmail.com>
2013-07-29 10:19:21 +0100

    version 1.01
    Fixed some basic issues. De-duplicated score calculation and simpler
    naming.

--- jscompare.js
+++ jscompare.js
@@ -1,13 +1,12 @@
 /*Global abatement */
 var jscompare = jscompare || {};
-jscompare.contentPrefs = jscompare.contentPrefs || {};
-jscompare.utils = jscompare.utils || {};
+jscompare = jscompare || {};
 
 /*Create instance for Levenshtein scoring */
-jscompare.contentPrefs.scoreObject = new diff_match_patch();
+jscompare.scoreObject = new diff_match_patch();
 
 /* Score weighting for individual word matches */
-jscompare.contentPrefs.compareWordsConf = {
+jscompare.compareWordsConf = {
 	weighting : 10,
 	rejectThreshold : 5
 }
@@ -16,18 +15,18 @@ jscompare.contentPrefs.compareWordsConf = {
 /* MAIN FUNCTION */
 /* ************* */
 
-jscompare.contentPrefs.analyseContent = function analyseContent(primaryString, secondaryString) {
+jscompare.analyseContent = function analyseContent(primaryString, secondaryString) {
 
 	/* Attributes */
 	this.primaryWordList = primaryString.split(' ');
 	this.secondaryWordList = secondaryString.split(' ');
 	this.primaryPhrasePermutations = {}
 	this.secondaryPhrasePermutations = {}
-	jscompare.contentPrefs.matchScore = null;
-	jscompare.contentPrefs.matchesList = {}
+	jscompare.matchScore = null;
+	jscompare.matchesList = {}
 
 	/* Methods */
-	
+
 	/* Natural logarithm - function obtained from internet but unable to relocate source */
 	this.ln = function ln(val) {
 		return Math.log(val) / Math.LOG10E;
@@ -35,41 +34,40 @@ jscompare.contentPrefs.analyseContent = function analyseContent(primaryString, s
 
 	this.getScore = function getScore(primarySubstring, secondarySubstring) {
 		/*Return Levenshtein score */
-		return jscompare.contentPrefs.scoreObject.diff_levenshtein(jscompare.contentPrefs.scoreObject.diff_main(primarySubstring, secondarySubstring));
+		return jscompare.scoreObject.diff_levenshtein(jscompare.scoreObject.diff_main(primarySubstring, secondarySubstring));
 	}
 	/* Return comparative scores on individual word or phrase combinations */
 	this.compareSubtext = function compareSubtext(primaryArray, secondaryArray) {
+		var scoreValue;
 		/* for each primary text related word */
 		primaryArray.forEach(function(primaryItem) {
 			/* compare that word with each secondary related word in turn and get a match scoring */
 			secondaryArray.forEach(function(secondaryItem) {
-				jscompare.contentPrefs.matchScore = that.getScore(primaryItem, secondaryItem);
+				jscompare.matchScore = that.getScore(primaryItem, secondaryItem);
 				/* check if the levenshtein score is low enough to meet the threshold for a useful match */
-				if (jscompare.contentPrefs.matchScore <= jscompare.contentPrefs.compareWordsConf.rejectThreshold) {
+				if (jscompare.matchScore <= jscompare.compareWordsConf.rejectThreshold) {
 					/* determine if match is new or scores less than an existing match */
-					if (jscompare.contentPrefs.matchesList[primaryItem] === undefined || (25 / that.ln(jscompare.contentPrefs.matchScore + 2)) * (that.ln(primaryItem.length)) > jscompare.contentPrefs.matchesList[primaryItem]) {
-						jscompare.contentPrefs.matchesList[primaryItem] = (25 / that.ln(jscompare.contentPrefs.matchScore + 2)) * (that.ln(primaryItem.length));
+					scoreValue = (25 / that.ln(jscompare.matchScore + 2)) * (that.ln(primaryItem.length));
+					if (jscompare.matchesList[primaryItem] === undefined || scoreValue > jscompare.matchesList[primaryItem]) {
+						jscompare.matchesList[primaryItem] = scoreValue;
 					}
 				}
 			})
 		})
 	}
-
 	var that = this;
 
 	/* Call driver program to return comparative scores */
 	this.compareSubtext(this.primaryWordList, this.secondaryWordList);
-	
-	return jscompare.contentPrefs.matchesList;
-}
 
+	return jscompare.matchesList;
+}
 /* Calculate the total of all the match scores as a final match total */
-jscompare.contentPrefs.CalculateCumulativeMatchScore = function CalculateCumulativeMatchScore(matchesListObject) {
+jscompare.CalculateCumulativeMatchScore = function CalculateCumulativeMatchScore(matchesListObject) {
 	this.runningScore = 0;
 	for (match in matchesListObject) {
 		this.runningScore += matchesListObject[match];
 	}
 	return this.runningScore;
 }
-
 
