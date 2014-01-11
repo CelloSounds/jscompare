9e6d075b53986112f2c0b5541421c959753dbf7c
Graham Ellsmore <graham@ellsmo.re>
2014-01-11 21:50:34 +0000

    Code clean and additional comments

--- jscompare.js
+++ jscompare.js
@@ -1,8 +1,8 @@
 /*
- * jscompare 1.03
+ * jscompare 1.04
  * Author: Graham Ellsmore (ellsmo.re)
  * Website: https://github.com/CelloSounds/jscompare
- * Date: 16-Nov-13
+ * Date: 11-Jan-14
  */
 
 /*Global abatement */
@@ -24,36 +24,39 @@ jscompare.compareWordsConf = {
 jscompare.analyseContent = function analyseContent(primaryString, secondaryString) {
 
 	/* Attributes */
-	this.primaryWordList = primaryString.split(' ');
-	this.secondaryWordList = secondaryString.split(' ');
-	this.primaryPhrasePermutations = {};
-	this.secondaryPhrasePermutations = {};
-	jscompare.matchScore = null;
+	var primaryWordList = primaryString.split(' ')
+	secondaryWordList = secondaryString.split(' ')
+	primaryPhrasePermutations = {}
+	secondaryPhrasePermutations = {}
+	jscompare.matchScore = null
 	jscompare.matchesList = {};
 
 	/* Methods */
 
-	/* Natural logarithm - function obtained from internet but unable to relocate source */
-	this.ln = function ln(val) {
+	/* Natural logarithm - from web */
+	var ln = function ln(val) {
 		return Math.log(val) / Math.LOG10E;
 	};
 
-	this.getScore = function getScore(primarySubstring, secondarySubstring) {
+	/* Function to request a levenshtein score for a word or phrase comparison 
+	 This is called multiple times untile all combinations between the 2 texts being compared are exhausted */ 
+	var getScore = function getScore(primarySubstring, secondarySubstring) {
 		/* Return Levenshtein score */
 		return jscompare.scoreObject.diff_levenshtein(jscompare.scoreObject.diff_main(primarySubstring, secondarySubstring));
 	};
-	/* Return comparative scores on individual word or phrase combinations */
-	this.compareSubtext = function compareSubtext(primaryArray, secondaryArray) {
+	
+	/* Iterate through each combination of individual words from both texts and request a match score if levenshtein score is within threshold */
+	var compareSubtext = function compareSubtext(primaryArray, secondaryArray) {
 		var scoreValue;
 		/* for each primary text related word */
 		primaryArray.forEach(function(primaryItem) {
 			/* compare that word with each secondary related word in turn and get a match scoring */
 			secondaryArray.forEach(function(secondaryItem) {
-				jscompare.matchScore = that.getScore(primaryItem, secondaryItem);
+				jscompare.matchScore = getScore(primaryItem, secondaryItem);
 				/* check if the levenshtein score is low enough to meet the threshold for a useful match */
 				if (jscompare.matchScore <= jscompare.compareWordsConf.rejectThreshold) {
 					/* Calculate match score value */
-					scoreValue = (25 / that.ln(jscompare.matchScore + 2)) * (that.ln(primaryItem.length));
+					scoreValue = (25 / ln(jscompare.matchScore + 2)) * (ln(primaryItem.length));
 					/* determine if match is new or scores less than an existing match */
 					if (jscompare.matchesList[primaryItem] === undefined || scoreValue > jscompare.matchesList[primaryItem]) {
 						jscompare.matchesList[primaryItem] = scoreValue;
@@ -62,19 +65,17 @@ jscompare.analyseContent = function analyseContent(primaryString, secondaryStrin
 			});
 		});
 	};
-	var that = this;
 
-	/* Call driver program to return comparative scores */
-	this.compareSubtext(this.primaryWordList, this.secondaryWordList);
+	/* Call primary function to return comparative scores */
+	compareSubtext(primaryWordList, secondaryWordList);
 
 	return jscompare.matchesList;
 };
 /* Calculate the total of all the match scores as a final match total */
-jscompare.CalculateCumulativeMatchScore = function CalculateCumulativeMatchScore(matchesListObject) {
-	this.runningScore = 0;
+jscompare.CalculateCumulativeMatchScore = function(matchesListObject) {
+	var runningScore = 0;
 	for (match in matchesListObject) {
-		this.runningScore += matchesListObject[match];
+		runningScore += matchesListObject[match];
 	}
-	return this.runningScore;
+	return runningScore;
 };
-
